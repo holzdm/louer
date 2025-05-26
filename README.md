ea4f7a779dcfd5119259a44911837c0dbc28c1ce
carol <carolfcassaro@gmail.com>
2025-05-25 21:46:36 -0300

    navbar quase pronta

--- README.md
+++ README.md
@@ -10,29 +10,6 @@ view: paginas html
 control: POST, validacoes, e funcoes
 model: conexao com o banco e comandos pro banco 
 
-<nav class="navbar" role="navigation" aria-label="main navigation">
 
-    <!-- lado esquerdo -->
-    <div class="navbar-brand navbar-start">
-      <a class="navbar-item" href="pag-inicial.php">
-        <img src="../a-imagens/louer-logo.png" alt="Logo">
-      </a>
-    </div>
+icon de coracao: <i class="fa-solid fa-heart"></i>
 
-    <!-- MENU -->
-    <div class="navbar-menu">
-      <!-- MENU: lado esquerdo (ver como vou colocar a barra do meio) -->
-      <div class="navbar-start">
-      </div>
-
-      <!-- MENU: lado direito (perfil) -->
-       <div class="navbar-end">
-          <div class="navbar-item has-dropdown is-hoverable">
-            <a class="navbar-link">
-              Perfil
-            </a>
-
-          </div>
-      </div>
-    </div>
-  </nav>
--- view/pag-inicial.php
+++ view/pag-inicial.php
@@ -1,56 +1,106 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link
-  rel="stylesheet"
-  href="https://cdn.jsdelivr.net/npm/bulma@1.0.4/css/bulma.min.css">
-  <link rel="stylesheet" href="styles.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.4/css/bulma.min.css">
+  <link rel="stylesheet" href="styles.css?v=1">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+  <script src="https://kit.fontawesome.com/414a446042.js" crossorigin="anonymous"></script>
   <title>Louer - Pagina Inicial</title>
 </head>
+
 <body>
   <nav class="navbar" role="navigation" aria-label="main navigation">
 
-    <!-- lado esquerdo -->
-    <div class="navbar-brand">
-      <a class="navbar-item" href="pag-inicial.php">
-        <img src="../a-imagens/louer-logo.png" alt="Logo">
+    <!-- LOGO-->
+    <div class="navbar-brand"> <!-- lado esquerdo -->
+      <a href="pag-inicial.php">
+        <img class="logo" src="../a-imagens/louer-logo.png" alt="Logo">
       </a>
     </div>
 
-    <!-- MENU -->
-    <div class="navbar-menu">
-      <!-- MENU: lado esquerdo (ver como vou colocar a barra do meio) -->
-      <div class="navbar-start">
-      </div>
-
-      <!-- MENU: lado direito (perfil) -->
-       <div class="navbar-end">
-          <div class="navbar-item">
-            <div class="dropdown">
-              <!-- botao do perfil -->
-              <div class="dropdown-trigger">
-                <button class="button" aria-haspopup="true" aria-controls="dropdown-menu">
-                  <span>Perfil</span>
-                  <span class="hamburger">
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                  </span>
-                </button>
-              </div>
-
-              <!-- conteudo dentro do dropdown do perfil -->
-               <div class="dropdown-menu">
-
-               </div>
-            </div>
+    <div class="navbar-meio">
+      <div class="barra-pesquisa">
+        <div class="categorias-dropdown" id="dropdown-categorias">
+          <div class="categorias-dropdown-trigger">
+            <button class="categorias-button">O que?</button>
           </div>
+        </div>
+        
+        <hr class="divisor-vertical">
+        <div class="localizacao">
+          Tem na minha cidade?
+          <div class="circulo-pesquisar"><i class="fa-solid fa-magnifying-glass"></i></div>
+        </div>
       </div>
     </div>
+
+    <div class="navbar-direita">
+      <div class="navbar-item">
+        <div class="dropdown is-right" id="dropdown-perfil">
+          <div class="dropdown-trigger">
+            <button class="button perfil-button" aria-haspopup="true" aria-controls="dropdown-menu">
+              <span class="icon is-small">
+                <i class="fa-solid fa-bars"></i>
+              </span>
+              <figure class="image is-32x32">
+                <!-- imagem dinamica -->
+                <img class="img-perfil" src="https://bulma.io/assets/images/placeholders/128x128.png" /> <!-- PEGAR IMAGEM INSERIDA NO FORMULARIO DO CADASTRO, MAX 128x128 -->
+              </figure>
+            </button>
+          </div>
+
+          <div class="dropdown-menu" id="dropdown-menu" role="menu">
+            <div class="dropdown-content">
+              <a href="#" class="dropdown-item">
+                Meu Perfil
+              </a>
+              <a href="#" class="dropdown-item">
+                Configurações
+              </a>
+              <hr class="dropdown-divider">
+              <a href="#" class="dropdown-item">
+                Sair
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
   </nav>
-  
+
+  <script>
+    const dropdownPerfil = document.getElementById('dropdown-perfil');
+    const dropdownTrigger = dropdownPerfil.querySelector('.dropdown-trigger');
+    const perfilButton = document.querySelector('.perfil-button');
+
+    const dropdownCategorias = document.getElementById('dropdown-categorias')
+
+    dropdownTrigger.addEventListener('click', function(event) {
+      event.stopPropagation(); // Impede que o clique suba para o document
+      if (dropdownPerfil.classList.contains('is-active')) {
+        dropdownPerfil.classList.remove('is-active');
+        perfilButton.blur();
+      } else {
+        dropdownPerfil.classList.add('is-active');
+        perfilButton.focus();
+      }
+    });
+
+    // Fechar se clicar fora
+    document.addEventListener('click', function(event) {
+      if (!dropdownPerfil.contains(event.target)) {
+        dropdownPerfil.classList.remove('is-active');
+        perfilButton.blur();
+      }
+    });
+
+
+
+  </script>
 </body>
+
 </html>
