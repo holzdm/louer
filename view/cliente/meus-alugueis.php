80d458ff8446e0f7acfcc2498893a88b9a04926e
carol <carolfcassaro@gmail.com>
2025-09-29 23:09:55 -0300

    mudanca simples

--- view/cliente/meus-alugueis.php
+++ view/cliente/meus-alugueis.php
@@ -7,7 +7,7 @@ $res = listarReservas($_SESSION['id']);
 ?>
 
 
-            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
+<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
     <?php while ($registro = mysqli_fetch_assoc($res)):
         $idReserva = $registro['id'];
         $idProduto = $registro['id_produto'];
@@ -17,19 +17,19 @@ $res = listarReservas($_SESSION['id']);
         $status = $registro['status'];
 
         $img = listarUmaImg($idProduto);
-        $srcImg = $img ? $img['url_img'] : '../../a-uploads/New-piskel.png';
+        $srcImg = $img ? '/louer/a-uploads/' . $img['url_img'] : '/louer/a-uploads/New-piskel.png';
 
     ?>
-    <div class="bg-white rounded-lg overflow-hidden h-60 flex flex-col shadow hover:shadow-lg">
-        <a href='../../control/ReservaController.php?acao=acessar&id=<?= $idReserva ?>'>
-            <img src='<?= $srcImg ?>' 
-                 alt='Imagem do produto' class="w-full h-48 object-cover">
-            <div class="p-4">
-                <h3 class="font-bold text-lg"><?= $nome ?></h3>
-                <p class="text-gray-600">R$<?= $valorReserva ?>/h</p>
-                <p class="text-sm mt-2"><?= $status ?></p>
-            </div>
-        </a>
-    </div>
+        <div class="bg-white rounded-lg overflow-hidden h-60 flex flex-col shadow hover:shadow-lg">
+            <a href='/louer/control/ReservaController.php?acao=acessar&id=<?= $idReserva ?>'>
+                <img src='<?= $srcImg ?>'
+                    alt='Imagem do produto' class="w-full h-30 object-cover">
+                <div class="p-4">
+                    <h3 class="font-bold text-lg"><?= $nome ?></h3>
+                    <p class="text-gray-600">R$<?= $valorReserva ?>/h</p>
+                    <p class="text-sm mt-2"><?= $status ?></p>
+                </div>
+            </a>
+        </div>
     <?php endwhile; ?>
 </div>
